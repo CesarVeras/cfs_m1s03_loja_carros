93de0c009c7c9c030dd9714955083d185ac4e13b
Eduardo Cesar Veras dos Santos <eduardocvsantos@gmail.com>
2023-02-20 22:21:32 -0300

    Resolvendo a questão 3.

--- index.js
+++ index.js
@@ -9,23 +9,33 @@ let veiculos = [];
 // 	console.log('Marca cadastrada com sucesso!');
 // }
 
-function cadastrarModelo(modelo, marca) {
-	if (marca === '' || modelo === '') return;
-	let obj = {
-		nome: modelo,
-		marca
-	}
-	modelos.push(obj);
-	console.log('Modelo cadastrado com sucesso!');
+// function cadastrarModelo(modelo, marca) {
+// 	if (marca === '' || modelo === '') return;
+// 	let obj = {
+// 		nome: modelo,
+// 		marca
+// 	}
+// 	modelos.push(obj);
+// 	console.log('Modelo cadastrado com sucesso!');
+// }
+
+function cadastrarVeiculo(modelo, marca, nome, km, valor, cor) {
+	let obj = { nome, marca, modelo, cor, km, valor	}
+	if (veiculos.some(v => v.nome === obj.nome)) return;
+	veiculos.push(obj);
+	console.log('Veículo cadastrado com sucesso!');
 }
 
 // cadastrarMarca('Fiat');
 // cadastrarMarca('BMW');
 // cadastrarMarca('Audi');
-cadastrarModelo('Uno', 'Fiat');
-cadastrarModelo('CRONOS', 'Fiat');
-cadastrarModelo('X5', 'BMW');
-cadastrarModelo('M5', 'BMW');
+// cadastrarModelo('Uno', 'Fiat');
+// cadastrarModelo('CRONOS', 'Fiat');
+// cadastrarModelo('X5', 'BMW');
+// cadastrarModelo('M5', 'BMW');
+cadastrarVeiculo('Uno', 'Fiat', 'Fiat Uno Vermelho', 0, 46840.00, 'Vermelho');
+cadastrarVeiculo('CRONOS', 'Fiat', 'Fiat CRONOS Branco', 0, 93843.00, 'Branco');
 
 // console.log(marcas);
-console.log(modelos);
+// console.log(modelos);
+console.log(veiculos);
