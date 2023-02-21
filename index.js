69836eaa8bfb857ea0c8584d652b0f49287c5458
Eduardo Cesar Veras dos Santos <eduardocvsantos@gmail.com>
2023-02-20 22:28:22 -0300

    Resolvendo a questão 4.

--- index.js
+++ index.js
@@ -2,28 +2,32 @@ let marcas = [];
 let modelos = [];
 let veiculos = [];
 
-// function cadastrarMarca(marca) {
-// 	if (marca === '') return;
-// 	if (marcas.some(m => m === marca)) return;
-// 	marcas.push(marca);	
-// 	console.log('Marca cadastrada com sucesso!');
-// }
+function cadastrarMarca(marca) {
+	if (marca === '') return;
+	if (marcas.some(m => m === marca)) return;
+	marcas.push(marca);	
+	console.log('Marca cadastrada com sucesso!');
+}
 
-// function cadastrarModelo(modelo, marca) {
-// 	if (marca === '' || modelo === '') return;
-// 	let obj = {
-// 		nome: modelo,
-// 		marca
-// 	}
-// 	modelos.push(obj);
-// 	console.log('Modelo cadastrado com sucesso!');
-// }
+function cadastrarModelo(modelo, marca) {
+	if (marca === '' || modelo === '') return;
+	let obj = {
+		nome: modelo,
+		marca
+	}
+	modelos.push(obj);
+	console.log('Modelo cadastrado com sucesso!');
+}
 
 function cadastrarVeiculo(modelo, marca, nome, km, valor, cor) {
 	let obj = { nome, marca, modelo, cor, km, valor	}
 	if (veiculos.some(v => v.nome === obj.nome)) return;
 	veiculos.push(obj);
 	console.log('Veículo cadastrado com sucesso!');
+}
+
+function listarVeiculos() {
+	return veiculos;
 }
 
 // cadastrarMarca('Fiat');
@@ -38,4 +42,4 @@ cadastrarVeiculo('CRONOS', 'Fiat', 'Fiat CRONOS Branco', 0, 93843.00, 'Branco');
 
 // console.log(marcas);
 // console.log(modelos);
-console.log(veiculos);
+console.log(listarVeiculos());
