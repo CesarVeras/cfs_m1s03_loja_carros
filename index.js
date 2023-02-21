4afcc0d6e5a82f3072a4190a0163b694301e6460
Eduardo Cesar Veras dos Santos <eduardocvsantos@gmail.com>
2023-02-21 00:02:44 -0300

    Resolvendo a questão 9.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-let marcas = [];
-let modelos = [];
-let veiculos = [];
+let marcasLocalStorage = JSON.parse(localStorage.getItem('marcas'));
+let modelosLocalStorage = JSON.parse(localStorage.getItem('modelos'));
+let veiculosLocalStorage = JSON.parse(localStorage.getItem('veiculos'));
+
+let marcas = marcasLocalStorage ? marcasLocalStorage : [];
+let modelos = modelosLocalStorage ? modelosLocalStorage : [];
+let veiculos = veiculosLocalStorage ? veiculosLocalStorage : [];
 
 function cadastrarMarca(marca) {
 	if (marca === '') return;
 	if (marcas.some(m => m === marca)) return;
 	marcas.push(marca);	
+	localStorage.setItem('marcas', JSON.stringify(marcas));
 	return 'Marca cadastrada com sucesso!';
 }
 
@@ -16,13 +21,15 @@ function cadastrarModelo(modelo, marca) {
 		marca
 	}
 	modelos.push(obj);
+	localStorage.setItem('modelos', JSON.stringify(modelos));
 	return 'Modelo cadastrado com sucesso!';
 }
 
-function cadastrarVeiculo(modelo, marca, nome, km, valor, cor) {
-	let obj = { nome, marca, modelo, cor, km, valor	}
+function cadastrarVeiculo(modelo, marca, nome, km, valor, cor, img) {
+	let obj = { nome, marca, modelo, cor, km, valor, img };
 	if (veiculos.some(v => v.nome === obj.nome)) return;
 	veiculos.push(obj);
+	localStorage.setItem('veiculos', JSON.stringify(veiculos));
 	return 'Veículo cadastrado com sucesso!';
 }
 
@@ -30,13 +37,13 @@ function listarVeiculos() {
 	return veiculos;
 }
 
-cadastrarMarca('Fiat');
-cadastrarMarca('BMW');
-cadastrarMarca('Audi');
-cadastrarModelo('Uno', 'Fiat');
-cadastrarModelo('CRONOS', 'Fiat');
-cadastrarModelo('X5', 'BMW');
-cadastrarModelo('M5', 'BMW');
+// cadastrarMarca('Fiat');
+// cadastrarMarca('BMW');
+// cadastrarMarca('Audi');
+// cadastrarModelo('Uno', 'Fiat');
+// cadastrarModelo('CRONOS', 'Fiat');
+// cadastrarModelo('X5', 'BMW');
+// cadastrarModelo('M5', 'BMW');
 // cadastrarVeiculo('Uno', 'Fiat', 'Fiat Uno Vermelho', 0, 46840.00, 'Vermelho');
 // cadastrarVeiculo('CRONOS', 'Fiat', 'Fiat CRONOS Branco', 0, 93843.00, 'Branco');
 
